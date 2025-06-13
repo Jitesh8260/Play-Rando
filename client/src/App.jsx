753adfc9cb79af8aa017dac0046ccce353f0c0f8
Jitesh8260 <jiteshbhakat16@gmail.com>
2025-06-13 23:07:19 +0530

    Intial Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Let&apos;s Rando!</h1>
+    </div>
+  );
+}
+
+export default App;
